64f4362e389dad4d1e4f6517b37498234745498f
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-08-21 13:10:47 +0700

    Test firebase db connection 8

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,1 +1,23 @@
 init();
+
+function savePolling(email, poll) {
+	firebase.database().ref('polling/').set({
+    'email': email,
+    'choice': poll    
+  }, function() {
+  	if(err){
+        alert("Your polling was not entered, please contact admin");
+  	} else {
+    	alert ("Your activity is submitted");        
+        $('#email-field').val('');
+        $('#polling-field').val('');
+  	}
+  });
+}
+
+$("#polling").submit(function(event) {	
+	var email = $("#email-field").val();
+	var poll = $("#polling-field").val();
+
+	savePolling(email, poll);
+});

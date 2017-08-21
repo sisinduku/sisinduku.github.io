6379316aa78bdb9e267f19c965b08ea06a948813
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-08-21 11:28:54 +0700

    Test firebase db connection 5

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,18 +1,10 @@
-function getConnection() {
-  // Set the configuration for your app
-  
-  var config = {
-    apiKey: "AIzaSyCxxS5-50vzHJMcHJTmf6mgu2xZTYywuE0",
-    authDomain: "github-pages-b413e.firebaseapp.com",
-    databaseURL: "https://github-pages-b413e.firebaseio.com/"    
-  };
-
-  var app = firebase.initializeApp(config);
-
-  console.log(app.name);
-
-  // Get a reference to the database service
-  var database = firebase.database();
-
-  return database;
-}
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCxxS5-50vzHJMcHJTmf6mgu2xZTYywuE0",
+  authDomain: "github-pages-b413e.firebaseapp.com",
+  databaseURL: "https://github-pages-b413e.firebaseio.com",
+  projectId: "github-pages-b413e",
+  storageBucket: "github-pages-b413e.appspot.com",
+  messagingSenderId: "121658623412"
+};
+firebase.initializeApp(config);

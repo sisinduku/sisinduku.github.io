1d91c8803d78e09d84842fddf3bf2c87538fc796
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-08-21 13:18:23 +0700

    Test firebase db connection 10

--- assets/js/db.js
+++ assets/js/db.js
@@ -18,7 +18,7 @@ function savePolling(email, poll) {
 $(document).ready(function($) {
 	$("#polling-sumbit").click(function() {	
 		var email = $("#email-field").val();
-		var poll = $("#polling-field").val();
+		var poll = $('input[name="polling-field"]:checked').val();
 
 		savePolling(email, poll);
 	});
